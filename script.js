0a2d887da7b3edf87540d608af6fa5a6c3761466
abturner002 <abturner002@converse.edu>
2018-10-18 15:58:30 -0400

    midterm background color for button and load fire stations not finished

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 $(function(){
-     // Add your javascript here
+     $('#buttondiv > input').on('click', 
+                   function(){
 
-         
-});
+  $(this).css("background-color", $(this).val())
+                               });
+
+    $(function insertJSON(urlStr, targetID) {
+            $.getJSON(urlStr, function(data) {
+                     
+                    
+                $.each(data, function(item) {
+                       
+                    });
+    });
+    $('#listdiv > input').on('click', insertJSON('GSP-Fire-Stations.json')); });
+ });
